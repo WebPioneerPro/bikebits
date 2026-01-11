c1fd753bcac873547da72e7ead2a9c94eb1f0f82
aruniPC2 <143891416+WebPioneerPro@users.noreply.github.com>
2026-01-11 22:33:37 +0530

    Added the BikeBits Logo

--- src/layout/AppHeader.tsx
+++ src/layout/AppHeader.tsx
@@ -30,17 +30,22 @@ const AppHeader: React.FC = () => {
     <header className="sticky top-0 flex w-full bg-white border-gray-200 z-99999 dark:border-gray-800 dark:bg-gray-900 lg:border-b">
       <div className="flex flex-col items-center justify-between grow lg:flex-row lg:px-6">
         <div className="flex items-center justify-between w-full gap-2 px-3 py-3 border-b border-gray-200 dark:border-gray-800 sm:gap-4 lg:justify-normal lg:border-b-0 lg:px-0 lg:py-4">
-          <Link to="/" className="lg:block">
+          <Link to="/" className="flex items-center gap-3">
             <img
               className="dark:hidden"
-              src="./images/logo/logo.svg"
+              src="./images/logo/bikebits-logo-light.png"
               alt="Logo"
+              width={40}
             />
             <img
               className="hidden dark:block"
-              src="./images/logo/logo-dark.svg"
+              src="./images/logo/bikebits-logo-dark.png"
               alt="Logo"
+              width={40}
             />
+            <span className="hidden text-xl font-semibold text-gray-800 sm:block dark:text-white/90">
+              BikeBits
+            </span>
           </Link>
 
           {/* <button
@@ -99,9 +104,8 @@ const AppHeader: React.FC = () => {
           </div> */}
         </div>
         <div
-          className={`${
-            isApplicationMenuOpen ? "flex" : "hidden"
-          } items-center justify-between w-full gap-4 px-5 py-4 lg:flex shadow-theme-md lg:justify-end lg:px-0 lg:shadow-none`}
+          className={`${isApplicationMenuOpen ? "flex" : "hidden"
+            } items-center justify-between w-full gap-4 px-5 py-4 lg:flex shadow-theme-md lg:justify-end lg:px-0 lg:shadow-none`}
         >
           <div className="flex items-center gap-2 2xsm:gap-3">
             {/* <!-- Dark Mode Toggler --> */}
